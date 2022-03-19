acd4eeded0eccf06365e4269a1bd2efc5d3f4180
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-19 15:31:36 +0500

    Create function operate

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function divide(a, b) {
     if (b === 0) return 'Divide by 0 is prohibited'
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}

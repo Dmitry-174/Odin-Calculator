f1fc7c3d3aa029e633979b65a03eacbb5a89183a
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-25 17:13:46 +0500

    Create functions showInputedValue, MakeCalculations and ShowResult

--- script.js
+++ script.js
@@ -27,3 +27,44 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+function showInputedValue() {
+    keyboardEl.addEventListener('click', (e) => {
+        if (!e.target.classList.contains('digit')) return;
+        inputedValue += e.target.textContent;
+        currentInput.textContent = inputedValue;
+    })
+} 
+
+function makeCalculations() {
+    keyboardEl.addEventListener('click', (e) => {
+        if (!e.target.classList.contains('operator')) return;
+        operator = e.target.textContent;
+        first = Number(inputedValue);
+        previousInput.textContent = `${first} ${operator}`;
+        inputedValue = '';
+    })
+}
+
+function ShowResult() {
+    keyboardEl.addEventListener('click', (e) => {
+        if (!e.target.classList.contains('equal')) return;
+        second = Number(inputedValue)
+        const result = operate(operator, first, second)
+        previousInput.textContent = `${first} ${operator} ${second} =`;
+        inputedValue = '';
+        currentInput.textContent = result;
+    })
+}
+
+let operator = '';
+let first = 0;
+let second = 0;
+let inputedValue = '';
+const keyboardEl = document.querySelector('.keyboard');
+const currentInput = document.querySelector('.current-input');
+const previousInput = document.querySelector('.previous-input');
+
+showInputedValue();
+makeCalculations();
+ShowResult();
